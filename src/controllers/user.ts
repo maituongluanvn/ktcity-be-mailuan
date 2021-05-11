5f00cd206ccbf4c58a85659d2e6ef397458f10e7
Luan Mai Tuong <luan.mai@spiritlabs.co>
2021-05-11 17:44:20 +0700

    connect database & install prisma & config prisma

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-const getUser = (req: Request, res: Response, next: NextFunction) => {
+const getUser = async (req: Request, res: Response, next: NextFunction) => {
+    // const { keyword: } = req.query;
+    // const user = await prisma.user.findUnique({
+    //     where: {
+    //         email: `%$as%`
+    //     }
+    // });
+    await prisma.$connect();
     return res.status(200).json({
         message: 'dep trai'
+        // data: user
     });
 };
 
